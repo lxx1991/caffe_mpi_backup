cba00954634df45ecd771ef1b36300d75937f431
lxx1991 <lxx1991@gmail.com>
2016-10-28 03:21:37 +0800

    fix examples

--- examples/IRes_VOC_mult_stage_N_Lconv/train.sh
+++ examples/IRes_VOC_mult_stage_N_Lconv/train.sh
@@ -4,4 +4,4 @@ GOOGLE_LOG_DIR=examples/IRes_VOC_mult_stage_N_Lconv/LOG \
     /usr/local/openmpi/bin/mpirun -np 4 \
     build/install/bin/caffe train \
     --solver=examples/IRes_VOC_mult_stage_N_Lconv/IRes_VOC_solver.prototxt \
-    --weights=examples/IRes_VOC_mult_stage_N_Lconv/IRes_VOC_mult_stage_N_10000.caffemodel
+    --weights=examples/IRes_VOC_mult_stage_N/model/unary_mat_iter_10000.caffemodel
